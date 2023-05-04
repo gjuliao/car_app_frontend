16957e50e61dfc83c94fa19dee8d83d7e0c5b67c
belaymit <2belamit@gmail.com>
2023-05-04 10:50:12 +0300

    Modified classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 import { Routes, Route } from 'react-router-dom'
+
+// components
 import Navbar from './components/Navbar'
+import SecureRoute from './components/SecureRoute'
+import Reservation from './components/Reservation'
+import MyReservation from './components/MyReservation'
+
+// pages
+import AddCar from './pages/AddCar'
+import Login from './pages/Login'
+import DeleteCar from './pages/DeleteCar'
+import Car from './pages/Car'
 import './App.css'
-function App() {
+
+const App = () => {
   return (
-   <div class="container">
+   <div className="container">
       <Navbar />
+     <div className="content">
      <Routes>
-      
+        <Route path="/login" element={<Login />}/>
+        <Route path="/cars" element={<Car/>} />
+        <Route element={<SecureRoute />}>
+          <Route path="/reservation" element={<Reservation />} />
+          <Route path="/reservation/:id" element={<Reservation />} />
+          <Route path="/my-reservation" element={<MyReservation />} />
+          <Route path="/add-car" element={<AddCar />} />
+          <Route path="/delete-car" element={<DeleteCar />} />
+        </Route>
      </Routes>
+     </div>
    </div>
   )
 }

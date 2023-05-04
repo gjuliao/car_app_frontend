f60c92286e495785ee5bbfdcb4bccf0db3d0a1bd
belaymit <2belamit@gmail.com>
2023-05-04 09:56:23 +0300

    Implemented Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import './App.css'
-
 function App() {
   return (
-   <>
+   <div class="container">
+      <Navbar />
      <Routes>
       
      </Routes>
-   </>
+   </div>
   )
 }
 

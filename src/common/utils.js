7ff69662b8944a584c6eca171887be09070f1050
belaymit <2belamit@gmail.com>
2023-05-04 14:25:14 +0300

    Added current year calculator for footer

---
+++ src/common/utils.js
@@ -0,0 +1,10 @@
+export const getCurrentDate = (separator='-') => {
+
+  let newDate = new Date()
+  let date = newDate.getDate();
+  let month = newDate.getMonth() + 1;
+  let year = newDate.getFullYear();
+  
+  return `${year}`
+  }
+  

53110b1d4ece0b0be35e2e60c9fe71762f16c6bf
Juank628 <jsanchez_zunino@yahoo.com>
2023-05-04 15:28:05 -0500

    Add styles for login mobile view

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import styles from '../styles/Login.module.css'
+import styles from '../styles/Login.module.css';
+import logo from '../assets/images/logo.png';
 
 const Login = () => (
   <section className={styles.onTopContainer}>
@@ -9,10 +10,12 @@ const Login = () => (
       </div>
       <div className={styles.formArea}>
         <form>
-          <h1>LOG IN</h1>
+          <img src={logo} />
+          <p>LOG IN</p>
           <input type="text" placeholder='User' />
           <input type="password" placeholder='Password' />
           <button>LOGIN</button>
+          <a href="/register">Create account</a>
         </form>
       </div>
     </div>

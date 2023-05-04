af552e07cae0b0a8464257f1355c2ebdc1bd98fe
Juank628 <jsanchez_zunino@yahoo.com>
2023-05-04 17:21:37 -0500

    Correct linters

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from '../styles/Registration.module.css';
 import logo from '../assets/images/logo.png';
-import { Link } from 'react-router-dom';
 
 const Registration = () => (
   <section className={styles.onTopContainer}>
-    <div className={styles.container}> 
+    <div className={styles.container}>
       <div className={styles.imageArea}>
         <img src="" />
       </div>
@@ -13,9 +13,9 @@ const Registration = () => (
         <form>
           <img src={logo} />
           <p>SIGN UP</p>
-          <input type="text" placeholder='User' />
-          <input type="password" placeholder='Password' />
-          <input type="password" placeholder='Confirm password' />
+          <input type="text" placeholder="User" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Confirm password" />
           <button>SIGNUP</button>
           <Link to="/login">Have a user?</Link>
         </form>

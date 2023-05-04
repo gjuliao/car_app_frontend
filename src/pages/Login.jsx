c2553f00f1ddc2146e250cf603459106feed078e
Juank628 <jsanchez_zunino@yahoo.com>
2023-05-04 14:56:26 -0500

    Position elements for mobile view

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import styles from '../styles/Login.module.css'
 
 const Login = () => (
-  <div>Login</div>
+  <section className={styles.onTopContainer}>
+    <div className={styles.container}> 
+      <div className={styles.imageArea}>
+        <img src="" />
+      </div>
+      <div className={styles.formArea}>
+        <form>
+          <h1>LOG IN</h1>
+          <input type="text" placeholder='User' />
+          <input type="password" placeholder='Password' />
+          <button>LOGIN</button>
+        </form>
+      </div>
+    </div>
+  </section>
 );
 
 export default Login;

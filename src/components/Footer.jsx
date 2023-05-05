2c6a6746d830ded81068cd39a063f621a98e90d7
Giovanni Juliao <gjuliao32@gmail.com>
2023-05-05 15:55:26 -0500

    Added footer component & css

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Footer.css';
+
+const Footer = () => (
+  <div className={styles.footer}>
+    <p>© 2023 Example Company. All rights reserved.</p>
+  </div>
+);
+
+export default Footer;

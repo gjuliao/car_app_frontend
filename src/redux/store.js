f24cdc21ab83b910b9761a6ac96f2ed4bc423f52
belaymit <2belamit@gmail.com>
2023-05-07 16:05:09 +0300

    Created reservaion slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import reservationReducer from './reservation/reservationSlice';
+import auth from './Auth/auth';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth,
+    reservations: reservationReducer,
+  },
 });
 
 export default store;

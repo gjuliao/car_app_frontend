c0447687efadb23b94072c6eda4a05c98270b692
Giovanni Juliao <gjuliao32@gmail.com>
2023-05-08 11:37:36 -0500

    Styled footer for desktop and mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,11 @@ import styles from '../styles/Footer.module.css';
 
 const Footer = () => (
   <div className={styles.footer}>
+    <ul className={styles.footerList}>
+      <li>My Reservations</li>
+      <li>Add Car</li>
+      <li>Delete Car</li>
+    </ul>
     <p>© 2023 Example Company. All rights reserved.</p>
   </div>
 );

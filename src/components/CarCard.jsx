71e8629c1524eb6a5a136f238b27006f249349bb
Giovanni Juliao <gjuliao32@gmail.com>
2023-05-09 15:43:21 -0500

    Aligned text in carousel cards

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -3,9 +3,9 @@ import bmw from '../assets/images/bmw.png';
 import styles from '../styles/Main.module.css';
 
 const CarCard = () => (
-    <div className={styles.carCard}>
-      <div className={styles.carBack}>
-        <img className={styles.car} src={bmw} alt="bmw-car" />
+  <div className={styles.carCard}>
+    <div className={styles.carBack}>
+      <img className={styles.car} src={bmw} alt="bmw-car" />
       </div>
       <div className={styles.carDescription}>
         <h4>BMW 946</h4>

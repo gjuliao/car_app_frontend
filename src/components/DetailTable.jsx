0d3322bf81a19d68774a421b550805d0daaf0ebb
Juank628 <jsanchez_zunino@yahoo.com>
2023-05-09 17:34:02 -0500

    Add real data to card detail page

--- src/components/DetailTable.jsx
+++ src/components/DetailTable.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import styles from '../styles/DetailTable.module.css'
+import React from 'react';
+import styles from '../styles/DetailTable.module.css';
 
-export default function DetailTable() {
+export default function DetailTable({ data }) {
   return (
     <table className={styles.detailTable}>
       <tbody>
         <tr>
           <td className={styles.keyRow}>Price</td>
-          <td className={styles.valueRow}>$26000</td>
+          <td className={styles.valueRow}>
+            $
+            {data?.price}
+          </td>
         </tr>
         <tr>
           <td className={styles.keyRow}>Brand</td>
-          <td className={styles.valueRow}>BMW</td>
+          <td className={styles.valueRow}>{data?.brand}</td>
         </tr>
         <tr>
           <td className={styles.keyRow}>Year</td>
-          <td className={styles.valueRow}>2020</td>
+          <td className={styles.valueRow}>{data?.year}</td>
         </tr>
         <tr>
           <td className={styles.keyRow}>Color</td>
-          <td className={styles.valueRow}>Red</td>
+          <td className={styles.valueRow}>{data?.color}</td>
         </tr>
-        
+
       </tbody>
     </table>
-  )
+  );
 }

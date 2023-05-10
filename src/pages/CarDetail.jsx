c467f69a69a818fe5e9652f39de53669ed2d73a0
Juank628 <jsanchez_zunino@yahoo.com>
2023-05-10 15:57:56 -0500

    Create styles for car details desktop view

--- src/pages/CarDetail.jsx
+++ src/pages/CarDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { getCar } from '../redux/carDetail/carDetailSlice';
 import DetailTable from '../components/DetailTable';
 import styles from '../styles/CarDetail.module.css';
@@ -22,16 +23,20 @@ export default function Detail() {
         </div>
       ) : (
         <section className={styles.container}>
-          <img className={styles.photo} src={data.payload?.image} alt="car" />
-          <div className={styles.info}>
-            <div>
+          <div className={styles.photoArea}>
+            <img className={styles.photo} src={data.payload?.image} alt="car" />
+          </div>
+          <div className={styles.infoArea}>
+            <div className={styles.features}>
               <h1 className={styles.title}>{data.payload?.model}</h1>
               <p className={styles.subtitle}>
                 {`- $${data.payload?.price * 0.1} deposit upon any ${data.payload?.model} purchase`}
               </p>
               <DetailTable data={data.payload} />
             </div>
-            <button type="button" className={styles.reserveBtn}>Reserve</button>
+            <Link to={`/reservation/${id}`}>
+              <button type="button" className={styles.reserveBtn}>Reserve</button>
+            </Link>
           </div>
         </section>
       )}

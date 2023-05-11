375b0bb6cf402356f40e92c5e9a08f811a084e22
belaymit <2belamit@gmail.com>
2023-05-11 15:02:19 +0300

    Added auth

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,11 @@ import reservationReducer from './reservation/reservationSlice';
 import sessionReducer from './sessionSlice';
 import carReducer from './carDetail/carDetailSlice';
 import carItemsReducer, { fetchCars } from './car/carSlice';
+import auth from './Auth/auth'
 
 const store = configureStore({
   reducer: {
+    auth,
     reservations: reservationReducer,
     session: sessionReducer,
     car: carReducer,

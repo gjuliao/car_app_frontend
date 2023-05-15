cb53d6e1ce78d2cdaee4a4ef37c10a4d3a979941
diego <dgcuenca@gmail.com>
2023-05-15 16:36:42 -0500

    Enable see list of car to be deleted

--- src/pages/DeleteCar.jsx
+++ src/pages/DeleteCar.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCars } from '../redux/carlistSlice';
+import { deleteCar } from '../redux/car/carSlice';
 
-const DeleteCar = () => (
-  <div>DeleteCar</div>
-);
+const DeleteCar = () => {
+  const cars = useSelector( store => store.cars);
+  const dispatch = useDispatch();
+  const deleteItem = (e) => {
+    dispatch(deleteCar(3))
+  }
+
+  useEffect(() => {
+    dispatch(fetchCars());
+  },[]);
+  return (
+    <ul>
+      { cars.list?.payload.map ( car => (
+        <li key = { car.id }>
+          <div>
+            {car.id}
+          </div>
+          <div>
+            {car.model}
+          </div>
+          <div>
+            <button name= {car.id} onClick = {deleteItem}>Delete Car</button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+};
 
 export default DeleteCar;

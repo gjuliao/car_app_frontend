5949336ad4e8f19ac4f9eec535dc9f568a5918be
diego <dgcuenca@gmail.com>
2023-05-16 11:10:09 -0500

    Style delete car page

--- src/pages/DeleteCar.jsx
+++ src/pages/DeleteCar.jsx
@@ -2,12 +2,13 @@ import React, { useEffect} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCars } from '../redux/carlistSlice';
 import { deleteCar } from '../redux/car/carSlice';
+import styles from '../styles/DeleteCar.module.css';
 
 const DeleteCar = () => {
   const cars = useSelector( store => store.cars);
   const dispatch = useDispatch();
   const deleteItem = (e) => {
-    dispatch(deleteCar(3))
+    dispatch(deleteCar(e.target.name))
   }
 
   useEffect(() => {

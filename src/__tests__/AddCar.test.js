f0b13a51e6b46b2f5f18112a2fd253e46ac22e94
Juank628 <jsanchez_zunino@yahoo.com>
2023-05-17 19:15:40 -0500

    Create components tests

--- src/__tests__/AddCar.test.js
+++ src/__tests__/AddCar.test.js
@@ -1,17 +1,41 @@
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import store from '../redux/store';
 import AddCar from '../pages/AddCar';
 
-test('AddCar renders without crashing', () => {
-  const tree = renderer.create(
-    <Provider store={store}>
-      <BrowserRouter>
-        <AddCar />
-      </BrowserRouter>
-    </Provider>,
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+describe('AddCar page tets', () => {
+
+    beforeEach(() => {
+        render(
+          <Provider store={store}>
+            <BrowserRouter>
+              <AddCar />
+            </BrowserRouter>
+          </Provider>,
+        );
+      });
+
+
+    test('display create button', () => {
+        expect(screen.getByText('Create')).toBeDefined();
+    })
+
+    test('Create button should be disabled', () => {
+        expect(screen.getByText('Create')).toBeDisabled();
+    })
+
+    test('AddCar renders without crashing', () => {
+        const tree = renderer.create(
+          <Provider store={store}>
+            <BrowserRouter>
+              <AddCar />
+            </BrowserRouter>
+          </Provider>,
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+      });
+})
